ca6a3b804d961408b0ca8ab9b718fa84e3e4e84c
StephanXV <stefa.florio@gmail.com>
2022-07-04 15:16:35 +0200

    q_table definition

--- ml_engine/rl_engine/agent.py
+++
@@ -1,7 +0,0 @@
-from typing import List
-
-
-class SelectionAgent:
-    """The agent that performs the instance selection exploring the world."""
-    def __init__(self, actions: List):
-        pass
--- ml_engine/rl_engine/state.py
+++
@@ -1,6 +0,0 @@
-class SelectionState:
-    """Representation of a state in the SelectionWorld"""
-    def __init__(self, rt_category: str, cpu_category: str, instance: str):
-        self.rt_category = rt_category
-        self.cpu_category = cpu_category
-        self.instance = instance
